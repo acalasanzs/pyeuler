99025ca1fe13e6a8cd2a63202881efe9bc07f0e1
Albert Calasanz <acalasanzs@gmail.com>
2022-04-24 12:54:42 +0200

    comments2

--- 782.py
+++ 782.py
@@ -48,37 +48,37 @@ def complexity(matrix):
             raise "not a ndarray"
     else:
         raise "not a ndarray"
-class c:
+class c:                                                #Create and object which will be used for contain all the values and its matrix in itself
     def __init__(self, n, k):
         self.n = n
         self.k = k
-        self.matrix = np.zeros((self.n,self.n))
-        for _ in range(self.k):
+        self.matrix = np.zeros((self.n,self.n))         #Initialize the void (n, n) matrix
+        for _ in range(self.k):                         #Add as ones as are required
             self.addOne()
     def addOne(self):
-        class point:
+        class point:                                    #Make an object which contains the point of a posible combination with its matrix complexity
             def __init__(self, x, y, complex):
                 self.x = x
                 self.y = y
                 self.complex = complex
-        temp = []
-        temp = np.copy(self.matrix)
-        mininum = []
+        temp = []                                       #Actual matrix to be changed
+        mininum = []                                    #list of all points wich will be tried
+        #Array indices
         x = 0
         y = 0
         while True:
-            temp = np.copy(self.matrix)
-            if x == self.n:
+            temp = np.copy(self.matrix)                 #Actual matrix to be changed copied from the original matrix
+            if x == self.n:                             #If x exceeds the last index, reset x and increase y if it's not the last index of y
                 x = 0
                 if y < self.n - 1:
                     y += 1
+                else:
+                    break                               #Break when it exceeds the last point of the matrix
             if temp[x][y] == 0:
                 temp[x][y] = 1
                 current_complexity = len(complexity(temp))
                 point1 = point(x, y, current_complexity)
                 mininum.append(point1)
-            if x == (self.n - 1) and y == (self.n - 1):
-                break
             x += 1
         mini = [co.complex for co in mininum]
         minI = mini.index(min(mini))
