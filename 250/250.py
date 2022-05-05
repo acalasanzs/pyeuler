1741860d3818a5b998725196300e73ab8a199964
Albert Calasanz <acalasanzs@gmail.com>
2022-05-05 12:47:06 +0200

    s

--- 250/250.py
+++ 250/250.py
@@ -7,18 +7,36 @@ def posible_sums(num):
         while divisor < num:
             current = num / divisor
             while not current.is_integer():
-                divisor +=1
+                divisor += 1
                 current = num / divisor
             divisor += 1
-            if current == 1:
-                break
             posibilities.append(int(current))
         return posibilities
-print(posible_sums(120))
 
 def to_exponent(base, exp):
     result = 1
     results = []
-    
-    for x in range(2, exp+1):
-        results.append((x, pow(base, x)))
+    exponents = posible_sums(exp)[::-1]
+    idx = 0
+    while idx < len(exponents)-1:
+        current = exponents[idx]
+        if idx > 0:
+            rest = exponents[idx+1] - current
+            match = -1
+            matches = []
+            for i in exponents:
+                match += 1
+                if rest == i:
+                    matches.append(True)
+                else:
+                    matches.append(False)
+            print(matches)
+            if any(matches):
+                results.append((exponents[match], pow(base, exponents[match])))
+            else:
+                results.append((current, pow(base, current)))
+        else:
+            results.append((current, pow(base, current)))
+        idx += 1
+    return results
+print(to_exponent(2,12))
