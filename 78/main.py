749204a19aee5588623092ef3f826b88deb9cf8f
Albert Calasanz <acalasanzs@gmail.com>
2022-05-09 13:55:01 +0200

    6th try

--- 78/main.py
+++ 78/main.py
@@ -3,18 +3,18 @@ def p(n):
     results = [first.copy()]                        #Total combinations list
     cantidad_de_vacios = 1                          #Cantidad de vacíos
     while cantidad_de_vacios < n:                   # Mientras la cantidad de vacios sea menor que n, es decir, todos los vacios disponibles
-        actual = first.copy()                       # Hacer una copia de la primera combinación para modificarla                          
+
+        """ Función que actualiza según los indices los vacios a añadir en first.copy() """
         def update(index):
-            global existent
+            actual = first.copy()                       # Hacer una copia de la primera combinación para modificarla
             for current in index:
                 actual.insert(current, 0)
             for arr in results:
                 try:
                     if all([True if arr[idx] == actual[idx] else False for idx in range(len(actual))]) or all([True if arr[idx] == actual[len(actual)-idx] else False for idx in range(len(actual))]):
-                        existent = True
                         return False
                     else:
-                        raise IndexError
+                        continue
                 except IndexError:
                     pass
             results.append(actual)
@@ -31,7 +31,7 @@ def p(n):
                 index[i] += 2
                 while index[i] in without_it:
                     index[i] += 2
-                if index[i] > len(actual)-1:
+                if index[i] > index[-1]:
                     index[i] = last
                     break
                 existent = update(index)
