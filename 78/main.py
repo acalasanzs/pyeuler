2108522abbd094fd107916ae4afb7a38e982d2a8
Albert Calasanz <acalasanzs@gmail.com>
2022-05-09 13:02:38 +0200

    second try

--- 78/main.py
+++ 78/main.py
@@ -3,11 +3,14 @@ def p(n):
     results = [first.copy()]
     cantidad_de_vacios = 1
     while cantidad_de_vacios < n:
-        idx = 1
         actual = first.copy()
-        while idx < cantidad_de_vacios*2:
-            actual.insert(idx, 0)
-            idx += 2
+        def update(index):
+            for current in index:
+                actual.insert(current, 0)
+        index = [i for i in range(1, cantidad_de_vacios*2, 2)]
+        update(index)
+        
+        
         results.append(actual)
         cantidad_de_vacios += 1
     return results
